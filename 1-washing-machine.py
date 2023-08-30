06cd05abdf204619ec19b5d9a80700d78285e212
hexadi <tawan204288@gmail.com>
2023-08-30 14:58:54 +0700

    add

--- 1-washing-machine.py
+++ 1-washing-machine.py
@@ -5,7 +5,7 @@ import asyncio
 import aiomqtt
 from enum import Enum
 
-student_id = "6300001"
+student_id = "6310301018"
 
 class MachineStatus(Enum):
     pressure = round(random.uniform(2000,3000), 2)
@@ -39,7 +39,7 @@ async def publish_message(w, client, app, action, name, value):
                 "value"     : value
             }
     print(f"{time.ctime()} - PUBLISH - [{w.SERIAL}] - {payload['name']} > {payload['value']}")
-    await client.publish(f"v1cdti/{app}/{action}/{student_id}/model-01/{w.SERIAL}"
+    await client.publish(f"v1cd/{app}/{action}/{student_id}/model-01/{w.SERIAL}"
                         , payload=json.dumps(payload))
 
 async def CoroWashingMachine(w, client):
@@ -48,16 +48,55 @@ async def CoroWashingMachine(w, client):
         wait_next = round(10*random.random(),2)
         print(f"{time.ctime()} - [{w.SERIAL}] Waiting to start... {wait_next} seconds.")
         await asyncio.sleep(wait_next)
+        if w.MACHINE_STATUS == 'OFF':
+            continue
+        if w.MACHINE_STATUS == 'ON':
+            
+            await publish_message(w, client, "hw", "get", "status", "START")
+
+            await publish_message(w, client, "hw", "get", "LID", "OPEN")
+
+            await publish_message(w, client, "hw", "get", "LID", "CLOSE")
+
+            # random status
+            status = random.choice(list(MachineStatus))
+            await publish_message(w, client, "app", "get", status.name, status.value)
+
+            await publish_message(w, client, "app", "get", "STATUS", "FINISHED")
+
+            # random maintenance
+            maint = random.choice(list(MachineMaintStatus))
+            await publish_message(w, client, "app", "get", maint.name, maint.value)
+            if (maint.name == 'noise' and maint.value == 'noisy'):
+                w.MACHINE_STATUS = 'OFF'
+                continue
+
+            await publish_message(w, client, "hw", "get", "status", "STOPPED")
+
+            await publish_message(w, client, "hw", "get", "status", "POWER OFF")
+            w.MACHINE_STATUS = 'OFF'
+
+            # random status
+
         
 
 async def listen(w, client):
     async with client.messages() as messages:
-        await client.subscribe(f"v1cdti/hw/set/{student_id}/model-01/{w.SERIAL}")
+        await client.subscribe(f"v1cd/hw/set/{student_id}/model-01/{w.SERIAL}")
+        async for message in messages:
+            m_decode = json.loads(message.payload)
+            if message.topic.matches(f"v1cd/hw/set/{student_id}/model-01/{w.SERIAL}"):
+                # set washing machine status
+                print(f"{time.ctime()} - MQTT - [{m_decode['serial']}]:{m_decode['name']} => {m_decode['value']}")
+                w.MACHINE_STATUS = 'ON'
         
 
 
 async def main():
     w = WashingMachine(serial='SN-001')
+
+    async with aiomqtt.Client("test.mosquitto.org") as client:
+        await asyncio.gather(listen(w, client) , CoroWashingMachine(w, client))
     
 
 asyncio.run(main())
